95d2601d5d8ffad76901a4c2845669106e54b1f6
Denys Mitiachkin <denismityachkin@gmail.com>
2022-03-28 23:12:11 +0300

    config file

--- server/app/config.js
+++ server/app/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     SMTP: {
-        SERVICE: process.env.SMTP_SERVICE || 'gmail',
-        USER: process.env.SMTP_USER || 'davinciQfficial@gmail.com',
-        PASSWORD: process.env.SMTP_PASSWORD || 'Victory1906',
-        MAILER: 'davinciQfficial@gmail.com'
+        SERVICE: process.env.SMTP_SERVICE,
+        USER: process.env.SMTP_USER,
+        PASSWORD: process.env.SMTP_PASSWORD,
+        MAILER: process.env.SMTP_MAILER
     },
 }
