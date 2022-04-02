0926145d07f250cbea0dbe51aff8676d7c13c752
Denys Mitiachkin <denismityachkin@gmail.com>
2022-04-03 00:49:49 +0300

    add react client

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Works fine!
+    </div>
+  )
+}
+
+export default App;
---
+++ client/src/constants.js
@@ -0,0 +1,3 @@
+export const LOGIN_ROUTE = '/login'
+export const REGISTRATION_ROUTE = '/registration'
+export const MAIN_ROUTE = '/'
