1738ef014c7fcaf735b5917410742908f838fcb2
Denys Mitiachkin <denismityachkin@gmail.com>
2022-04-16 19:08:13 +0300

    http requests for authentication

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,8 +1,15 @@
 import axios from 'axios'
+import jwt_decode from 'jwt-decode'
 
-// TODO fix export
+
 const host = axios.create({
     // TODO Take local network ip from server side and replace with baseURL
+    baseURL: 'http://localhost:8000' || 'http://192.168.0.100:8000',
+    timeout: 1000,
+    headers: {'X-Custom-Header': 'foobar'}
+})
+
+const authHost = axios.create({
     baseURL: 'http://localhost:8000' || 'http://192.168.0.100:8000',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
@@ -14,7 +21,6 @@ const dictionaryId = '726d6368-80bd-4820-9d11-bc43fc215d47'
 const createEntry = async (key, value) => {
     return await host.post(`/users/${userId}/dictionaries/${dictionaryId}/entries/`,
         {key, value})
-
 }
 
 const getRandomOne = async () => {
@@ -22,8 +28,31 @@ const getRandomOne = async () => {
 }
 
 
+
+const registration = async (email, password) => {
+    const {data} = await host.post('/user/registration', {email, password})
+    localStorage.setItem('token', data.token)
+    return jwt_decode(data.token)
+}
+
+const login = async (email, password) => {
+    const {data} = await host.post('/user/login', {email, password})
+    localStorage.setItem('token', data.token)
+    return jwt_decode(data.token)
+}
+
+const check = async () => {
+    const {data} = await authHost.get('api/user/auth' )
+    localStorage.setItem('token', data.token)
+    return jwt_decode(data.token)
+}
+
+
 export {
     host,
     createEntry,
-    getRandomOne
+    getRandomOne,
+    registration,
+    login,
+    check
 }
