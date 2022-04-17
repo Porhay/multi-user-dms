a6c679687308e2102b5bbbae9f8357c01debf66f
Denys Mitiachkin <denismityachkin@gmail.com>
2022-04-17 22:19:04 +0300

    refactoring

--- client/src/pages/Dictionaries.js
+++ client/src/pages/Dictionaries.js
@@ -1,13 +1,61 @@
-import React from 'react'
-import {Container} from "react-bootstrap";
+import React, {useEffect, useState} from 'react'
+import {Button, Container, Form, ListGroup} from 'react-bootstrap'
+import {useNavigate} from "react-router-dom";
 
-import Dictionaries from "../components/Dictionaries";
+import {createDictionary, getDictionaries} from '../http'
+import {ROUTES} from "../constants";
+
+
+const userId = '726d6368-80bd-4820-9d11-bc43fc215d47'
 
 
 const DictionariesPage = () => {
+    const navigate = useNavigate()
+
+    const [data, setData] = useState([])
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        updateData(userId)
+    }, [])
+
+    const updateData = (userId) => {
+        getDictionaries(userId).then((response) => {
+            setData(response.data)
+        })
+    }
+
+    const newDictionary = async () => {
+        await createDictionary({userId, name})
+        updateData(userId)
+        setName('')
+    }
+
+    const openDictionary = async (dictionaryId) => {
+        navigate(ROUTES.ENTRIES + '/' + dictionaryId)
+    }
+
     return (
-        <Container className="d-flex flex-column mt-3">
-            <Dictionaries />
+        <Container className="d-flex flex-column">
+            <Form>
+                <Form.Control
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    placeholder={"Type dictionary name here..."}
+                />
+            </Form>
+            <Button variant="outline-success" onClick={newDictionary}>New Dictionary</Button>
+
+            <ListGroup>
+                {data.map((item) => {
+                    return (
+                        <ListGroup.Item key={item.id} action onClick={() => openDictionary(item.id)}>
+                            {item.name}
+                        </ListGroup.Item>
+                    )
+                })}
+            </ListGroup>
+
         </Container>
     )
 }
