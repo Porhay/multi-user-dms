25c934cffbfe735f1e253cba704e3e06e85bff53
Denys Mitiachkin <denismityachkin@gmail.com>
2022-04-17 19:32:30 +0300

    add dictionary list

--- client/src/components/Dictionaries.js
+++ client/src/components/Dictionaries.js
@@ -1,17 +1,30 @@
-import React, {useState} from 'react'
-import {Button, Container, Form} from 'react-bootstrap'
-import {createDictionary} from '../http'
+import React, {useEffect, useState} from 'react'
+import {Button, Container, Form, ListGroup} from 'react-bootstrap'
+import {createDictionary, getDictionaries} from '../http'
 
 
 const Dictionaries = () => {
-    const [data, setData] = useState('')
+    const [data, setData] = useState([])
     const [name, setName] = useState('')
 
+    const userId = '726d6368-80bd-4820-9d11-bc43fc215d47'
     const newDictionary = async () => {
-        const userId = '726d6368-80bd-4820-9d11-bc43fc215d47'
         await createDictionary({userId, name})
         setName('')
+
+        let response = await getDictionaries(userId)
+        setData(response.data)
     }
+
+    const alertClicked = async (dictionaryId) => {
+        alert(`You clicked ${dictionaryId}`)
+    }
+
+    // TODO investigate useEffect fetch
+    // useEffect(async () => {
+    //     let response = await getDictionaries(userId)
+    //     setData(response.data)
+    // })
 
     return (
         <Container className="d-flex flex-column">
@@ -23,6 +36,17 @@ const Dictionaries = () => {
                 />
             </Form>
             <Button variant="outline-success" onClick={newDictionary}>New Dictionary</Button>
+
+            <ListGroup defaultActiveKey="#link1">
+                {data.map((item) => {
+                    return (
+                        <ListGroup.Item key={item.id} action onClick={() => alertClicked(item.id)}>
+                            {item.name}
+                        </ListGroup.Item>
+                    )
+                })}
+            </ListGroup>
+
         </Container>
     )
 }
