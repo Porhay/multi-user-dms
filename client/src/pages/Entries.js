bfa4a90fd589469dc1e92cf17a547c4c1607dbec
Denys Mitiachkin <denismityachkin@gmail.com>
2022-04-17 21:36:01 +0300

    entries list bug fixes

--- client/src/pages/Entries.js
+++ client/src/pages/Entries.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react'
-import {Container, ListGroup} from 'react-bootstrap'
-import { getEntries } from "../http";
-import { useParams } from 'react-router-dom';
+import {Button, Container, Form, ListGroup} from 'react-bootstrap'
+import {createEntry, getEntries} from "../http";
+import {useParams} from 'react-router-dom';
 
 
 const EntriesPage = () => {
-    const {id} = useParams()
+    const {id: dictionaryId} = useParams() // dictionaryId
+    const userId = 'random'
 
     const [data, setData] = useState([])
     const updateData = (userId, dictionaryId) => {
@@ -16,17 +17,41 @@ const EntriesPage = () => {
 
     useEffect(() => {
         // TODO fix userId
-        updateData('random', id)
+        updateData(userId, dictionaryId)
     }, [])
 
 
+    const [key, setKey] = useState('')
+    const [value, setValue] = useState('')
+    const newEntry = async () => {
+        await createEntry({userId, dictionaryId, key, value})
+        setKey('')
+        setValue('')
+        updateData(userId, dictionaryId)
+    }
+
+    // TODO Fix ListGroup.Item output, make cards
     return (
         <Container className="d-flex flex-column">
+            <Form>
+                <Form.Control
+                    value={key}
+                    onChange={e => setKey(e.target.value)}
+                    placeholder={"Type entry name here..."}
+                />
+                <Form.Control
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    placeholder={"Type entry value here..."}
+                />
+            </Form>
+            <Button variant="outline-success" onClick={newEntry}>New Entry</Button>
+
             <ListGroup>
                 {data.map((item) => {
                     return (
                         <ListGroup.Item key={item.id}>
-                            {item.key}
+                            {item.key}  ===> {item.value}
                         </ListGroup.Item>
                     )
                 })}
