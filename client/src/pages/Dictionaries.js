b80ecdd3c3efcabcbb7a44fa3961011affbdf41e
Denys Mitiachkin <denismityachkin@gmail.com>
2022-04-19 22:50:25 +0300

    customize, restyling

--- client/src/pages/Dictionaries.js
+++ client/src/pages/Dictionaries.js
@@ -7,6 +7,8 @@ import {createDictionary, getDictionaries} from '../http'
 import {ROUTES} from "../constants";
 import {Context} from "../index";
 
+import '../styles/Form.css';
+
 
 // const userId = '726d6368-80bd-4820-9d11-bc43fc215d47'
 
@@ -41,26 +43,38 @@ const DictionariesPage = observer(() => {
     }
 
     return (
-        <Container className="d-flex flex-column">
-            <Form>
-                <Form.Control
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    placeholder={"Type dictionary name here..."}
-                />
-            </Form>
-            <Button variant="outline-success" onClick={newDictionary}>New Dictionary</Button>
-
-            <ListGroup>
-                {data.map((item) => {
-                    return (
-                        <ListGroup.Item key={item.id} action onClick={() => openDictionary(item.id)}>
-                            {item.name}
-                        </ListGroup.Item>
-                    )
-                })}
-            </ListGroup>
-
+        <Container className="d-flex flex-column w-75">
+            <Container className="d-flex flex mt-5 mb-1">
+                <Form
+                    className="w-100"
+                >
+                    <Form.Control
+                        size="md"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        placeholder={"Type dictionary name here..."}
+                    />
+                </Form>
+                <Button size="md" variant="outline-success" onClick={newDictionary}>New</Button>
+            </Container>
+            <Container
+                className="justify-content-center"
+            >
+                <ListGroup>
+                    {data.map((item) => {
+                        return (
+                            <ListGroup.Item
+                                size="sm"
+                                key={item.id}
+                                action
+                                onClick={() => openDictionary(item.id)}
+                            >
+                                {item.name}
+                            </ListGroup.Item>
+                        )
+                    })}
+                </ListGroup>
+            </Container>
         </Container>
     )
 })
--- client/src/pages/Entries.js
+++ client/src/pages/Entries.js
@@ -3,6 +3,9 @@ import {Button, Container, Form, ListGroup} from 'react-bootstrap'
 import {createEntry, getEntries} from "../http";
 import {useParams} from 'react-router-dom';
 
+import '../styles/Form.css';
+
+
 
 const EntriesPage = () => {
     const {id: dictionaryId} = useParams() // dictionaryId
@@ -32,30 +35,38 @@ const EntriesPage = () => {
 
     // TODO Fix ListGroup.Item output, make cards
     return (
-        <Container className="d-flex flex-column">
-            <Form>
-                <Form.Control
-                    value={key}
-                    onChange={e => setKey(e.target.value)}
-                    placeholder={"Type entry name here..."}
-                />
-                <Form.Control
-                    value={value}
-                    onChange={e => setValue(e.target.value)}
-                    placeholder={"Type entry value here..."}
-                />
-            </Form>
-            <Button variant="outline-success" onClick={newEntry}>New Entry</Button>
-
-            <ListGroup>
-                {data.map((item) => {
-                    return (
-                        <ListGroup.Item key={item.id}>
-                            {item.key}  ===> {item.value}
-                        </ListGroup.Item>
-                    )
-                })}
-            </ListGroup>
+        <Container className="d-flex flex-column w-75">
+            <Container className="d-flex mt-5 mb-1">
+                <Form
+                    className="d-flex flex-row w-100"
+                >
+                    <Form.Control
+                        size="md"
+                        value={key}
+                        onChange={e => setKey(e.target.value)}
+                        placeholder={"Name"}
+                    />
+                    <Form.Control
+                        size="md"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        placeholder={"Value"}
+                    />
+                </Form>
+                <Button size="md" variant="outline-success" onClick={newEntry}>New</Button>
+            </Container>
+            <Container className="d-flex flex-row">
+                <ListGroup className="w-100">
+                    {data.map((item) => {
+                        return (
+                            <ListGroup.Item key={item.id}>
+                                <h6 className="mb-0">{item.key}</h6>
+                                <div className="div">{item.value}</div>
+                            </ListGroup.Item>
+                        )
+                    })}
+                </ListGroup>
+            </Container>
         </Container>
     )
 }
