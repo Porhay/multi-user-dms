6451e03d71063ced70dadc571bab5899a20c7dd9
Denys Mitiachkin <denismityachkin@gmail.com>
2024-02-19 05:22:10 +0200

    update production setting for db

--- server/app/db.js
+++ server/app/db.js
@@ -1,56 +1,59 @@
 'use strict'
 
-import {DataTypes, Sequelize} from 'sequelize'
+import { DataTypes, Sequelize } from 'sequelize'
 import * as constants from './lib/constants.js'
 
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME || 'root',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASSWORD || 'root',
+    process.env.DATABASE_URL, // Render.com provides a DATABASE_URL environment variable
     {
         dialect: 'postgres',
-        host: 'dms-postgresql', // process.env.LOCALHOST || process.env.REMOUTE_HOST || '0.0.0.0', // TODO: fix!
-        port: parseInt(process.env.DB_PORT || '5432')
+        ssl: process.env.DB_ENABLE_SSL, // Enable SSL for secure connections
+        dialectOptions: {
+            ssl: process.env.DB_ENABLE_SSL && {
+                require: true,
+                rejectUnauthorized: false, // Disable SSL certificate validation
+            }
+        },
     }
-)
+);
 
 export const user = sequelize.define('user', {
-    id: {type: DataTypes.UUID, primaryKey: true, allowNull: false},
-    email: {type: DataTypes.STRING, allowNull: false},
-    emailVerified: {type: DataTypes.BOOLEAN, defaultValue: false},
-    username: {type: DataTypes.STRING, allowNull: false, unique: true},
-    password: {type: DataTypes.STRING, allowNull: false},
-    role: {type: DataTypes.STRING, defaultValue: constants.USER_ROLES.USER},
-    friends: {type: DataTypes.JSON, defaultValue: []},
-    name: {type: DataTypes.STRING},
-    image: {type: DataTypes.STRING},
-}, {underscored: true})
+    id: { type: DataTypes.UUID, primaryKey: true, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    emailVerified: { type: DataTypes.BOOLEAN, defaultValue: false },
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, defaultValue: constants.USER_ROLES.USER },
+    friends: { type: DataTypes.JSON, defaultValue: [] },
+    name: { type: DataTypes.STRING },
+    image: { type: DataTypes.STRING },
+}, { underscored: true })
 
 
 export const verificationCodes = sequelize.define('verificationCodes', {
-    id: {type: DataTypes.UUID, primaryKey: true, allowNull: false},
-    userId: {type: DataTypes.UUID, allowNull: false},
-    code: {type: DataTypes.STRING},
-    used: {type: DataTypes.BOOLEAN, defaultValue: false},
-    createdAt: {type: DataTypes.TEXT},
-    expiresAt: {type: DataTypes.TEXT}
-}, {underscored: true, updatedAt: false, createdAt: false})
+    id: { type: DataTypes.UUID, primaryKey: true, allowNull: false },
+    userId: { type: DataTypes.UUID, allowNull: false },
+    code: { type: DataTypes.STRING },
+    used: { type: DataTypes.BOOLEAN, defaultValue: false },
+    createdAt: { type: DataTypes.TEXT },
+    expiresAt: { type: DataTypes.TEXT }
+}, { underscored: true, updatedAt: false, createdAt: false })
 
 
 export const dictionaries = sequelize.define('dictionaries', {
-    id: {type: DataTypes.UUID, primaryKey: true, allowNull: false},
-    userId: {type: DataTypes.UUID, allowNull: false},
-    name: {type: DataTypes.TEXT, defaultValue: 'My dictionary'},
-    type: {type: DataTypes.TEXT, defaultValue: constants.DICTIONARY_TYPES.STANDARD},
-    count: {type: DataTypes.INTEGER, defaultValue: 0},
-}, {underscored: true})
+    id: { type: DataTypes.UUID, primaryKey: true, allowNull: false },
+    userId: { type: DataTypes.UUID, allowNull: false },
+    name: { type: DataTypes.TEXT, defaultValue: 'My dictionary' },
+    type: { type: DataTypes.TEXT, defaultValue: constants.DICTIONARY_TYPES.STANDARD },
+    count: { type: DataTypes.INTEGER, defaultValue: 0 },
+}, { underscored: true })
 
 
 export const entries = sequelize.define('entries', {
-    id: {type: DataTypes.UUID, primaryKey: true, allowNull: false},
-    dictionaryId: {type: DataTypes.UUID, allowNull: false},
-    key: {type: DataTypes.TEXT},
-    value: {type: DataTypes.TEXT},
-    image: {type: DataTypes.STRING},
-}, {underscored: true})
+    id: { type: DataTypes.UUID, primaryKey: true, allowNull: false },
+    dictionaryId: { type: DataTypes.UUID, allowNull: false },
+    key: { type: DataTypes.TEXT },
+    value: { type: DataTypes.TEXT },
+    image: { type: DataTypes.STRING },
+}, { underscored: true })
